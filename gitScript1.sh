957cecb26bf638f8255add6815a6928cdd5caa3d
Jakedez <jakedez7@gmail.com>
2026-05-11 17:34:00 -0600

    Completed gitTest

---
+++ gitScript1.sh
@@ -0,0 +1,8 @@
+git init
+git branch -m master main
+git add helloworld.py
+git commit
+git branch branch1
+git switch branch1
+git add git_check.py
+git commit
---
+++ gitScript2.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/BYUComputingBootCampTests/git-clone.git
+cd git-clone/
+git log
+git diff e4c1d b1cab
+git restore --source b1cab door.py
+git add door.py
+git commit
